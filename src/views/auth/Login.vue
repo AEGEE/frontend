f67a33c920ed173e9da2a6dafbcfc09385141073
Rik Smale <WikiRik@users.noreply.github.com>
2020-06-03 11:17:48 +0200

    chore: improved notice on login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -4,8 +4,8 @@
     <div class="column is-6 is-offset-3">
       <div class="box">
         <div class="notification is-warning">
-          We are migrating our data to a new internal system. <br/>
-          In case you are experiencing issues, please contact us at <a href="//aegee.org/helpdesk">helpdesk</a>.
+          We just migrated to a new internal system, which might have some bugs we have not yet discovered. <br/>
+          In case you are experiencing issues, please contact us at <a href="//aegee.org/helpdesk">helpdesk</a> or press the 'Report bug!' button on the bottom right.
         </div>
         <div v-show="error" style="color:red; word-wrap:break-word;">{{ error }}</div>
         <form v-on:submit.prevent="login">
