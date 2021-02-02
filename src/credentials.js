5efbbfbcb4068ca85aeeacf1c58aa34677660c70
Rik Smale <WikiRik@users.noreply.github.com>
2021-02-02 16:35:05 +0100

    chore(map): return API key

--- src/credentials.js
+++ src/credentials.js
@@ -1,3 +1,3 @@
 export default {
-  MAPS_API_TOKEN: 'https://api.maptiler.com/maps/basic/style.json?key='
+  MAPS_API_TOKEN: 'https://api.maptiler.com/maps/basic/style.json?key=owErAUsX1uv65c8aUM4y'
 }
