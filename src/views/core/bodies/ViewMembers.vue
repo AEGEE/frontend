5ab7f542c09ddbb592a93df58114324424492c59
WikiRik <WikiRik@users.noreply.github.com>
2021-03-20 21:25:27 +0000

    chore(lint): fix linter

--- src/views/core/bodies/ViewMembers.vue
+++ src/views/core/bodies/ViewMembers.vue
@@ -161,7 +161,7 @@ export default {
           services: this.services,
           showError: this.$root.showError,
           showSuccess: this.$root.showSuccess,
-          router: this.$router,
+          router: this.$router
         }
       })
     },
