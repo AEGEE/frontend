60b19d8f8dea60a070da6d7344eb34139cfcb25d
WikiRik <WikiRik@users.noreply.github.com>
2023-08-16 18:41:23 +0000

    fix(deps): update to buefy 0.9

--- src/views/statutory/Applications.vue
+++ src/views/statutory/Applications.vue
@@ -54,63 +54,62 @@
           default-sort="statutory_id"
           default-sort-direction="desc"
           @sort="onSort">
-          <template slot-scope="props">
-            <b-table-column field="statutory_id" label="#" numeric sortable>
-              {{ props.row.statutory_id }}
-            </b-table-column>
+          <b-table-column field="statutory_id" label="#" numeric sortable v-slot="props">
+            {{ props.row.statutory_id }}
+          </b-table-column>
 
-            <b-table-column field="user_id" label="User ID" sortable>
-              {{ props.row.user_id }}
-            </b-table-column>
+          <b-table-column field="user_id" label="User ID" sortable v-slot="props">
+            {{ props.row.user_id }}
+          </b-table-column>
 
-            <b-table-column field="body_name" label="Body" centered sortable>
-              {{ props.row.body_name }}
-            </b-table-column>
+          <b-table-column field="body_name" label="Body" centered sortable v-slot="props">
+            {{ props.row.body_name }}
+          </b-table-column>
 
-            <b-table-column field="is_on_memberslist" label="Is on memberslist?" centered sortable :visible="event.type === 'agora'">
-              <span :class="calculateClassForMemberslist(props.row)">
-                {{ props.row.is_on_memberslist | beautify }}
-              </span>
-            </b-table-column>
+          <b-table-column field="is_on_memberslist" label="Is on memberslist?" centered sortable :visible="event.type === 'agora'" v-slot="props">
+            <span :class="calculateClassForMemberslist(props.row)">
+              {{ props.row.is_on_memberslist | beautify }}
+            </span>
+          </b-table-column>
 
-            <!-- important: there should be no whitespaces/line breaks inside this tag, as it messes up the white-space: pre-wrap styling. !-->
-            <b-table-column
-              v-for="(field, index) in fields"
-              :visible="selectedFields.some(sField => sField.name === field.name)"
-              v-bind:key="index"
-              field="answers[index]"
-              class="has-text-pre-wrap"
-              :label="field.name">{{ field.get(props.row) | beautify }}</b-table-column>
+          <!-- important: there should be no whitespaces/line breaks inside this tag, as it messes up the white-space: pre-wrap styling. !-->
+          <b-table-column
+            v-for="(field, index) in fields"
+            :visible="selectedFields.some(sField => sField.name === field.name)"
+            v-bind:key="index"
+            field="answers[index]"
+            class="has-text-pre-wrap"
+            :label="field.name"
+            v-slot="props">{{ field.get(props.row) | beautify }}</b-table-column>
 
-            <b-table-column field="cancelled" label="Cancelled?" centered :visible="displayCancelled">
-              {{ props.row.cancelled | beautify }}
-            </b-table-column>
+          <b-table-column field="cancelled" label="Cancelled?" centered :visible="displayCancelled" v-slot="props">
+            {{ props.row.cancelled | beautify }}
+          </b-table-column>
 
-            <b-table-column field="confirmed" label="Confirmed?" centered>
-              {{ props.row.confirmed | beautify }}
-            </b-table-column>
+          <b-table-column field="confirmed" label="Confirmed?" centered v-slot="props">
+            {{ props.row.confirmed | beautify }}
+          </b-table-column>
 
-            <b-table-column field="attended" label="Attended?" centered>
-              {{ props.row.attended | beautify }}
-            </b-table-column>
+          <b-table-column field="attended" label="Attended?" centered v-slot="props">
+            {{ props.row.attended | beautify }}
+          </b-table-column>
 
-            <b-table-column label="View" centered>
-              <router-link :to="{ name: 'oms.statutory.applications.view', params: { id: event.url || event.id, application_id: props.row.statutory_id || props.row.id } }">
-                View
-              </router-link>
-            </b-table-column>
+          <b-table-column label="View" centered v-slot="props">
+            <router-link :to="{ name: 'oms.statutory.applications.view', params: { id: event.url || event.id, application_id: props.row.statutory_id || props.row.id } }">
+              View
+            </router-link>
+          </b-table-column>
 
-            <b-table-column label="Manage status" field="status" centered sortable>
-              <div class="select" :class="{ 'is-loading': props.row.isSaving }">
-                <select v-model="props.row.newStatus" @change="switchPaxStatus(props.row)">
-                  <option value="pending">Pending</option>
-                  <option value="accepted">Accepted</option>
-                  <option value="waiting_list">Waiting list</option>
-                  <option value="rejected">Rejected</option>
-                </select>
-              </div>
-            </b-table-column>
-          </template>
+          <b-table-column label="Manage status" field="status" centered sortable v-slot="props">
+            <div class="select" :class="{ 'is-loading': props.row.isSaving }">
+              <select v-model="props.row.newStatus" @change="switchPaxStatus(props.row)">
+                <option value="pending">Pending</option>
+                <option value="accepted">Accepted</option>
+                <option value="waiting_list">Waiting list</option>
+                <option value="rejected">Rejected</option>
+              </select>
+            </div>
+          </b-table-column>
 
           <template slot="empty">
             <empty-table-stub />
--- src/views/statutory/BoardView.vue
+++ src/views/statutory/BoardView.vue
@@ -51,6 +51,7 @@
           </ul>
         </div>
 
+        <!-- TODO: fix this table, something about being in the same div height is likely an issue -->
         <div v-show="scope === 'edit' && selectedBody">
           <div class="notification is-danger" v-if="Object.keys(this.errors).length > 0">
             <div class="content">
@@ -66,26 +67,24 @@
 
           <b-table
             :data="bodyStatuses">
-            <template slot-scope="props">
-              <b-table-column label="Participant type and order">
-                {{ props.row.participant_type }} ({{ props.row.participant_order }})
-              </b-table-column>
+            <b-table-column label="Participant type and order" v-slot="props">
+              {{ props.row.participant_type }} ({{ props.row.participant_order }})
+            </b-table-column>
 
-              <b-table-column field="user_id" label="User">
-                <div class="select">
-                  <select v-model="props.row.user_id">
-                    <option :value="null">Not set</option>
-                    <option v-for="application in applications" v-bind:key="application.user_id" v-bind:value="application.user_id">
-                      {{ application.first_name }} {{ application.last_name }}
-                    </option>
-                  </select>
-                </div>
-              </b-table-column>
+            <b-table-column field="user_id" label="User" v-slot="props">
+              <div class="select">
+                <select v-model="props.row.user_id">
+                  <option :value="null">Not set</option>
+                  <option v-for="application in applications" v-bind:key="application.user_id" v-bind:value="application.user_id">
+                    {{ application.first_name }} {{ application.last_name }}
+                  </option>
+                </select>
+              </div>
+            </b-table-column>
 
-              <b-table-column field="board_comment" label="Board comment" sortable>
-                <textarea class="textarea" v-model="props.row.board_comment" />
-              </b-table-column>
-            </template>
+            <b-table-column field="board_comment" label="Board comment" sortable v-slot="props">
+              <textarea class="textarea" v-model="props.row.board_comment" />
+            </b-table-column>
           </b-table>
 
           <div class="notification is-warning">
@@ -138,48 +137,46 @@
             :loading="isLoading"
             paginated
             :per-page="limit">
-            <template slot-scope="props">
-              <b-table-column field="statutory_id" label="#" numeric>
-                {{ props.row.statutory_id }}
-              </b-table-column>
+            <b-table-column field="statutory_id" label="#" numeric v-slot="props">
+              {{ props.row.statutory_id }}
+            </b-table-column>
 
-              <b-table-column field="last_name" label="First and last name">
-                {{ props.row.first_name }} {{ props.row.last_name }}
-              </b-table-column>
+            <b-table-column field="last_name" label="First and last name" v-slot="props">
+              {{ props.row.first_name }} {{ props.row.last_name }}
+            </b-table-column>
 
-              <b-table-column label="Participant type and order">
-                <span v-if="props.row.participant_type">{{ props.row.participant_type }} ({{ props.row.participant_order }})</span>
-              </b-table-column>
+            <b-table-column label="Participant type and order" v-slot="props">
+              <span v-if="props.row.participant_type">{{ props.row.participant_type }} ({{ props.row.participant_order }})</span>
+            </b-table-column>
 
-              <b-table-column field="is_on_memberslist" label="Is on memberslist?" centered :visible="event.type === 'agora'">
-                <span :class="calculateClassForMemberslist(props.row)">
-                  {{ props.row.is_on_memberslist | beautify }}
-                </span>
-              </b-table-column>
+            <b-table-column field="is_on_memberslist" label="Is on memberslist?" centered :visible="event.type === 'agora'" v-slot="props">
+              <span :class="calculateClassForMemberslist(props.row)">
+                {{ props.row.is_on_memberslist | beautify }}
+              </span>
+            </b-table-column>
 
-              <b-table-column field="cancelled" label="Cancelled?" centered :visible="displayCancelled">
-                <span class="tag" :class="{ 'is-danger': props.row.cancelled }">
-                  {{ props.row.cancelled | beautify }}
-                </span>
-              </b-table-column>
+            <b-table-column field="cancelled" label="Cancelled?" centered :visible="displayCancelled" v-slot="props">
+              <span class="tag" :class="{ 'is-danger': props.row.cancelled }">
+                {{ props.row.cancelled | beautify }}
+              </span>
+            </b-table-column>
 
-              <b-table-column label="View" centered>
-                <router-link :to="{ name: 'oms.statutory.applications.view', params: { id: event.url || event.id, application_id: props.row.statutory_id || props.row.id } }">
-                  View
-                </router-link>
-              </b-table-column>
+            <b-table-column label="View" centered v-slot="props">
+              <router-link :to="{ name: 'oms.statutory.applications.view', params: { id: event.url || event.id, application_id: props.row.statutory_id || props.row.id } }">
+                View
+              </router-link>
+            </b-table-column>
 
-              <b-table-column field="status" label="Status" centered>
-                <span v-if="props.row.status === 'accepted'">Accepted</span>
-                <span v-if="props.row.status === 'rejected'">Rejected</span>
-                <span v-if="props.row.status === 'waiting_list'">Waiting list</span>
-                <span v-if="props.row.status === 'pending'">Pending</span>
-              </b-table-column>
+            <b-table-column field="status" label="Status" centered v-slot="props">
+              <span v-if="props.row.status === 'accepted'">Accepted</span>
+              <span v-if="props.row.status === 'rejected'">Rejected</span>
+              <span v-if="props.row.status === 'waiting_list'">Waiting list</span>
+              <span v-if="props.row.status === 'pending'">Pending</span>
+            </b-table-column>
 
-              <b-table-column v-for="(field, index) in selectedFields" v-bind:key="index" field="answers[index]" :label="field.name">
-                {{ field.get(props.row) | beautify }}
-              </b-table-column>
-            </template>
+            <b-table-column v-for="(field, index) in selectedFields" v-bind:key="index" field="answers[index]" :label="field.name" v-slot="props">
+              {{ field.get(props.row) | beautify }}
+            </b-table-column>
 
             <template slot="empty">
               <empty-table-stub />
